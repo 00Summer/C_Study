736f9bb8d507a866bfe96c52a64f2fc124a3f823
00Summer <710392739@qq.com>
2022-01-15 14:23:12 +0800

    C语言初学习

---
+++ newstudy.c
@@ -0,0 +1,16 @@
+#include<stdio.h>//#include表示包含 stdio.h--标准库--standard input output
+
+int main()//int整型 main主函数--有且仅有一个，是程序执行的入口
+{
+	printf("hello new world");//printf--库函数，使用前要声明，即#include<stdio.h>。
+
+	printf("%d\n", sizeof(char));//1  sizeof 的单位为byte字节
+	printf("%d\n", sizeof(short));//2
+	printf("%d\n", sizeof(int));//4
+	printf("%d\n", sizeof(long));//4
+	printf("%d\n", sizeof(long long));//8
+	printf("%d\n", sizeof(float));//4
+	printf("%d\n", sizeof(double));//8
+
+	return 0;//因为主函数是整型 结束要返回一个整型值
+}
